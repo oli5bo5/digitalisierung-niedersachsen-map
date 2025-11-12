b5b50f953f6d1914edab238a3ce474c83fcb9355
oli5bo5 <pruban@web.de>
2025-11-12 12:05:48 +0100

    fix: Configure Next.js for static export (Cloudflare Pages)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    return config;
+  },
+  // Cloudflare Pages Konfiguration
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
